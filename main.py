dc44ef403f76ae0667195f6d944210dd45e3fecc
higlander-cha <77839534+higlander-cha@users.noreply.github.com>
2021-02-04 11:52:24 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -3,41 +3,40 @@ import math
 
 filename = "SampleCoordinates.txt"
 
-#Måste vara integers för att funktionerna ska fungera
-def x_factor(b):
-    x = b * math.pi/180
-    return x
+
+def read_coordinare_file(filename):
+    # Funktioner för att räkna om värderna {a,b} till Mercator projection
+    def x_factor(b):
+        x = b * math.pi/180
+        return x
+
+    def y_factor(a):
+        y = np.log(math.tan((math.pi/4) + (a * math.pi/360)))
+        return y
+
+    x_lst = list()
+    y_lst = list()
+
+    with open(filename) as file:
+        # Öppnar angivna filen
+        for line in file:
+            # Ta bort klutter och konvertera till Mercator projection
+            line = line[1:7]
+            a, b = line.split(',') # HÄR är separeringen jag behövde för att få scriptet att fungera.
+            a = float(a)
+            b = float(b)
+            a = y_factor(a)
+            b = x_factor(b)
+            x_lst.append(a)
+            y_lst.append(b)
+
+    coord = np.array([x_lst, y_lst])
+    # print(coord)
+    # print(x_lst)
+    # print(type(x_lst))
+    # print(y_lst)
+
+    return coord
 
 
-def y_factor(a):
-    y = np.log(math.tan((math.pi/4) + (a * math.pi/360)))
-    return y
-
-# def read_coordinare_file(filename):
-
-
-lst = ''
-
-
-with open(filename) as file:
-    print(type(file))
-
-# tar bort klutter
-    for line in file:
-        line = line.replace('}', '')
-        line = line.replace('{', '')
-        line = line.replace(' ', '')
-        lst = lst + line
-#får ut en sträng som ser bra ut
-print(type(lst))
-print(lst)
-
-#Delar upp strängen i en lista med coordinaterna.
-lst_split = lst.split('\n')
-print(type(lst_split))
-print(lst_split)
-
-
-
-
-
+print((read_coordinare_file(filename)))
