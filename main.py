da0fff7ab785d4681a9d68e143770ac5a0c2f7ec
higlander-cha <77839534+higlander-cha@users.noreply.github.com>
2021-02-07 12:11:09 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import numpy as np
 import math
+# %config InlineBackend.figure_formats = ['svg']
+import matplotlib.pyplot as plt
+plt.rcParams['figure.figsize'] = [5, 3]
 
 filename = "SampleCoordinates.txt"
 
 
-def read_coordinare_file(filename):
+def read_coordinate_file(filename):
     # Funktioner för att räkna om värderna {a,b} till Mercator projection
     def x_factor(b):
         x = b * math.pi/180
@@ -22,21 +25,67 @@ def read_coordinare_file(filename):
         for line in file:
             # Ta bort klutter och konvertera till Mercator projection
             line = line[1:7]
-            a, b = line.split(',') # HÄR är separeringen jag behövde för att få scriptet att fungera.
-            a = float(a)
-            b = float(b)
-            a = y_factor(a)
-            b = x_factor(b)
-            x_lst.append(a)
-            y_lst.append(b)
+            a, b = line.split(',')  # HÄR är separeringen jag behövde för att få scriptet att fungera.
+            x_lst.append(x_factor(float(b)))
+            y_lst.append(y_factor(float(a)))
 
-    coord = np.array([x_lst, y_lst])
+    coord_list = np.array([x_lst, y_lst])
     # print(coord)
     # print(x_lst)
     # print(type(x_lst))
     # print(y_lst)
 
-    return coord
+    return coord_list
+
+coord_list = read_coordinate_file(filename)
+# print((read_coordinate_file(filename)))
+
+def plot_points(coord_list):
+    sz = coord_list.shape[1]
+    n = np.linspace(1, sz, sz)
+    fig, ax = plt.subplots(1, figsize=(10, 6))
+    fig.suptitle('Koordinater')
+    coord_x, coord_y = np.split(read_coordinate_file(filename), 2)
+    plt.scatter(coord_x, coord_y)
+    coord_x = coord_x.reshape(sz, 1)
+    coord_y = coord_y.reshape(sz, 1)
+    for k, txt in enumerate(n):
+        plt.annotate(txt-1, (coord_x[k], coord_y[k]))
+    plt.show()
 
 
-print((read_coordinare_file(filename)))
+# plot_points(read_coordinate_file(filename))
+
+# def construct_graph_connections(coord_list, radius):
+
+
+lst = []
+radius = 0.1
+print(coord_list)
+
+
+coord_list = coord_list.reshape((coord_list.shape[1], 2))
+
+for X, element1 in enumerate(coord_list):
+    print(element1[1])
+
+    for Y, element2 in enumerate(coord_list):
+
+        r = (((element2[0] - element1[0]) ** 2) + element2[1] - element1[1] ** 2) ** 0.5
+        if abs(r) < radius:
+            lst.append([X, element1, Y, element2, r])
+        else:
+            continue
+print(lst)
+
+#Här blir det fel med
+# for nr, element in enumerate(coord_list):
+#     x = element[0]
+#     y = ele
+#     # r = (((element2[0]-element1[0])**2) + element2[1]-element1[1]**2) ** 0.5
+#     # if abs(r) < radius:
+#     #     lst.append([X, element1, Y, element2, r])
+#     # else:
+#     #     continue
+# print(lst)
+
